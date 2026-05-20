dbec7d811f3ee47d393b5f5c248398033dedd80f
tiendung85 <tien.dungg2011@gmail.com>
2026-05-20 19:16:55 +0700

    Thêm chức năng thêm sản phẩm

--- lab1_prm/lib/models/product.dart
+++ lab1_prm/lib/models/product.dart
@@ -3,4 +3,15 @@ class Product {
   late String name;
   late double price;
   late int quantity;
+
+  static List<Product> products = [];
+
+  static void addProduct(int id, String name, double price, int quantity) {
+    Product product = Product();
+    product.id = id;
+    product.name = name;
+    product.price = price;
+    product.quantity = quantity;
+    products.add(product);
+  }
 }
